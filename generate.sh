60b91821f7cf4dd72dd98c084a3230c7d2439d86
David Howden <dhowden@gmail.com>
2019-03-06 15:47:04 +1100

    moving to version 2

--- generate.sh
+++ generate.sh
@@ -17,20 +17,8 @@ done
 
 rm -fr sajari/
 
-pkgrepo_prefix="code.sajari.com/protogen-go"
-
-pkg_map=""
-pkg_map="$pkg_map,Msajari/rpc/status.proto=$pkgrepo_prefix/sajari/rpc"
-pkg_map="$pkg_map,Msajari/rpc/empty.proto=$pkgrepo_prefix/sajari/rpc"
-pkg_map="$pkg_map,Msajari/engine/value.proto=$pkgrepo_prefix/sajari/engine"
-pkg_map="$pkg_map,Msajari/engine/key.proto=$pkgrepo_prefix/sajari/engine"
-pkg_map="$pkg_map,Msajari/engine/query/query.proto=$pkgrepo_prefix/sajari/engine/query"
-pkg_map="$pkg_map,Msajari/engine/query/v1/query.proto=$pkgrepo_prefix/sajari/engine/query/v1"
-pkg_map="$pkg_map,Msajari/api/query/v1/query.proto=$pkgrepo_prefix/sajari/api/query/v1"
-pkg_map="$pkg_map,Msajari/engine/store/record/record.proto=$pkgrepo_prefix/sajari/engine/store/record"
-
 echo 1>&2 "Building protos:"
 for dir in $(find $PROTO_SRC -name '*.proto' | xargs -n1 dirname | sort | uniq); do
   echo 1>&2 "- $dir"
-  protoc -I$PROTO_SRC --go_out=plugins=grpc$pkg_map:. $dir/*.proto
+  protoc -I$PROTO_SRC --go_out=paths=source_relative,plugins=grpc:. $dir/*.proto
 done
